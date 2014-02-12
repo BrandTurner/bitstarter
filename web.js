8ef807831b4c5ba019a4eec94a753d236bdec416
Brandon Turner <bturner@gmail.com>
2014-02-12 04:44:10 +0000

    Rar

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2 !');
+  var buffer = fs.readFileSync('index.html');
+  var text = buffer.toString('utf-8');
+
+  response.send('Grrrr!');
 });
 
 var port = process.env.PORT || 5000;
